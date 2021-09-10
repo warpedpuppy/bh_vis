9dc93d3d4993a4b50760f5a49c93d22606334b3f
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-09-10 06:42:21 -0400

    added event handler

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -22,8 +22,8 @@ export class MainView extends React.Component {
 
               return (
             <div className="main-view">
-                <button onClick={() => {alert('Nice!')}}>Click me!</button>
                 {movies.map(movie => <MovieCard key={movie._id} movie={movie}/>)}
+                onMovieClick={newSelectedMovie => { this.setState({ selectedMovie: newSelectedMovie }); }}  
             </div>
         );
         
