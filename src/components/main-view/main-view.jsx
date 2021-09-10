1b2e4a5e0feef1114394aceee5e3c9c8ba40f5ab
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-09-10 06:27:34 -0400

    imported movie view and button

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
 
@@ -13,12 +14,15 @@ export class MainView extends React.Component {
     
     render() {
         
-            const { movies } = this.state;
+            const { movies, selectedMovie } = this.state;
+
+            if (selectedMovie) return <MovieView movie={selectedMovie} />;
 
             if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
 
               return (
             <div className="main-view">
+                <button onClick={() => {alert('Nice!')}}>Click me!</button>
                 {movies.map(movie => <MovieCard key={movie._id} movie={movie}/>)}
             </div>
         );
