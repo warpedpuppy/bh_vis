45be4dec7735e1bc81c45eac473ac117581f0344
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-09-11 06:18:06 -0400

    array of movies added

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -7,7 +7,30 @@ export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
-            movies: [ ],
+            movies: [
+                {
+                     _id: 1,
+                    Title: "The Last Dance",
+                    ImagePath: "https://en.wikipedia.org/wiki/File:The_Last_Dance_2020.jpg",
+                    Genre: "Documentary",
+                    Description: "is a non-fiction movie that documents reality for education."
+                },
+                {
+                    _id: 2,
+                    Title: "Training Day",
+                    ImagePath: "https://upload.wikimedia.org/wikipedia/en/b/b3/Training_Day_Poster.jpg",
+                    Genre: "Crime thriller focuses on exciting elements of crimes.",
+                    Description: "Police drama about a veteran cop who escorts a rookie cop on his first day with the LAPD tough inner city narcotics unit.",
+                },
+                {
+                    _id: 3,
+                    Title: "Equalizer",
+                    ImagePath: "https://en.wikipedia.org/wiki/Nomadland_(film)#/media/File:Nomadland_poster.jpeg",
+                    Genre: "Action an exciting fast pace film with special effects.",
+                    Description: "a man with mysterious origin tries to put his past behind him and live a quiet new life.",
+                }
+
+            ],
             selectedMovie: null
         };
     }
