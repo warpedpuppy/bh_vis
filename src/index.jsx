e9da511c902a8d62c6e90d74ee4b9e8406167422
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-09-12 08:31:32 -0400

    created a container for grid

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MainView } from './components/main-view/main-view';
+import Container from 'react-bootstrap/Container';
 
 import './index.scss';
 
 class MyFlixApplication extends React.Component {
     render() {
         return (
-            <MainView />
+            <Container>
+              <MainView />
+            </Container>
         );
     }
 }
