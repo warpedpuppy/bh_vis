ddf64a5b99c1cdb757e1c9baee235915b193ec5e
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-09-19 11:10:25 -0400

    updated users state

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,28 +1,45 @@
 import React, { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export function LoginView(props) {
     const [ username, setUsername ] = useState('');
     const [ password, setPassword ] = useState('');
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(username, password);
-        /* then call props.onLoggedIn(username) */
-        props.onLoggedIn(username);
+        /*Send a request to the server for authentication */
+        axios.post('https://stormy-taiga-55813.herokuapp.com/login', {
+            Username: username,
+            Password: password
+        })
+        .then(response => {
+            const data = response.data;
+            props.onLoggedIn(data);
+        })
+        .catch(e => {
+            console.log('no such user')
+        });
     };
+        /* console.log(username, password);
+         then call props.onLoggedIn(username) 
+        props.onLoggedIn(username);
+    };*/
 
     return (
         <form>
-            <label>
-                Username:
-                <input type="text" value={username} onChange={e =>
-                setUsername(e.target.value)} />
-            </label>
-            <label>
-                Password:
-                <input type="pasword" value={password} onChange={e => setPassword(e.target.value)} />
-            </label>
-            <button type="submit" onClick={handleSubmit}>Submit</button>
+            <Form.Group controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            </Form.Group> 
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+                Submit
+            </Button>
         </form>
     );
 }
