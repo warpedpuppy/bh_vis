6d69f38c5bb6d92456246fc2d9190a2f1a0ab494
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-09-22 16:17:02 -0400

    added crossOrgin to display pictures

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -9,7 +9,7 @@ export class MovieCard extends React.Component {
 
         return (
             <Card>
-                <Card.Img variant="top" src={movie.ImagePath} />
+                <Card.Img crossOrigin="anonymous" variant="top" src={movie.ImagePath} />
                 <Card.Body>
                     <Card.Title>{movie.Title}</Card.Title>
                     <Card.Text>{movie.Description}</Card.Text>
