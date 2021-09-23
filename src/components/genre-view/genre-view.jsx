bd78d95470d9ae54c79999df83658a521e87c8ec
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-09-23 18:07:08 -0400

    update profile

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 
 export class GenreView extends React.Component {
