b81e20263cdd4e7088661a402372113222c893e8
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-09-28 09:37:44 -0400

    added reducers

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,9 +2,15 @@ export const SET_Movies = 'SET_MOVIES';
 export const SET_FILTER = 'SET_FILTER';
 
 export function setMovies(value) {
-    return { type: SET_Movies, value };
+    return { 
+        type: SET_Movies, 
+        value 
+    };
 }
 
 export function setFilter(value) {
-    return { type: SET_FILTER, value };
+    return { 
+        type: SET_FILTER, 
+        value 
+    };
 }
