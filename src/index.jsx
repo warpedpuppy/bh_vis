42b0a0be91450e601ded8b26fc1f04a110259590
harrisb8 <82327189+harrisb8@users.noreply.github.com>
2021-09-28 10:06:19 -0400

    added store to index

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MainView } from './components/main-view/main-view';
 import Container from 'react-bootstrap/Container';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
+import { MainView } from './components/main-view/main-view';
 
 import './index.scss';
+
+const store = createStore(moviesApp);
 
 class MyFlixApplication extends React.Component {
     render() {
         return (
-            <Container>
-              <MainView />
-            </Container>
+            <Provider store={store}>
+              <Container>
+                <MainView />
+              </Container>
+            </Provider>
         );
     }
 }
 
+//Find the root of our app
 const container = document.getElementsByClassName('app-container')[0];
 
+//Tell react to render our app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
